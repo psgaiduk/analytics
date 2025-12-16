624dfdce998bca9a308b0684c63571148e56cd4f
pavel <psgaiduk@gmail.com>
2025-12-16 15:20:38 +0300

    #fix: fix problem with work airflow

--- airflow/entrypoint-airflow.sh
+++ airflow/entrypoint-airflow.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 set -e
 
-# Инициализация базы
-airflow db init
+airflow db migrate
 
-# Создание админа
-airflow users create \
-  --username "${AIRFLOW_ADMIN_USER}" \
-  --password "${AIRFLOW_ADMIN_PASSWORD}" \
-  --firstname Admin \
-  --lastname User \
-  --role Admin \
-  --email "${AIRFLOW_ADMIN_EMAIL}"
+# Создаём admin пользователя ТОЛЬКО для webserver
+if [ "$1" = "webserver" ]; then
+  echo "Checking if admin user exists..."
 
-# Запуск webserver
-exec airflow webserver
+  if ! airflow users list | grep -q "^${AIRFLOW_ADMIN_USER}\b"; then
+    echo "Creating admin user..."
+    airflow users create \
+      --username "${AIRFLOW_ADMIN_USER}" \
+      --password "${AIRFLOW_ADMIN_PASSWORD}" \
+      --firstname Admin \
+      --lastname User \
+      --role Admin \
+      --email "${AIRFLOW_ADMIN_EMAIL}"
+  else
+    echo "Admin user already exists"
+  fi
+fi
+
+case "$1" in
+  webserver)
+    exec airflow webserver
+    ;;
+  scheduler)
+    exec airflow scheduler
+    ;;
+  *)
+    exec airflow "$@"
+    ;;
+esac
