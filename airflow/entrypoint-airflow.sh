fd48d8bba3a848f0e780fb0bcc1c801d930c93b6
pavel <psgaiduk@gmail.com>
2025-12-18 15:31:28 +0300

    #feat: add password in env for airflow

--- airflow/entrypoint-airflow.sh
+++ airflow/entrypoint-airflow.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
 
-# Миграция базы (при старте контейнера)
+# --- Генерация пароля admin для Simple Auth Manager ---
+PASSWORD_FILE="/opt/airflow/simple_auth_data/simple_auth_manager_passwords.json"
+mkdir -p "$(dirname "$PASSWORD_FILE")"
+
+if [ ! -f "$PASSWORD_FILE" ]; then
+    echo "{\"admin\": \"${AIRFLOW_ADMIN_PASSWORD}\"}" > "$PASSWORD_FILE"
+    echo "Создан файл с паролем admin"
+else
+    echo "Файл с паролем уже существует, пропускаем создание"
+fi
+
+# --- Миграция базы (при старте контейнера) ---
 airflow db migrate
 
-# Запуск нужного сервиса
+# --- Запуск нужного сервиса ---
 case "$1" in
     api-server)
         exec airflow api-server
