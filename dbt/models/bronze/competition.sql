5f273d3d2e3a8a46737590d4151176b2f8e4ee8a
pavel <psgaiduk@gmail.com>
2026-01-07 10:21:10 +0300

    #feat: create bronze competition sql

---
+++ dbt/models/bronze/competition.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized='view'
+) }}
+
+SELECT
+    RaceId AS race_id,
+    km AS km,
+    catId As cat_id,
+    DisciplineId AS discipline_id,
+    CAST(StatusId AS Int64) AS status_id,
+    StatusText AS status_text,
+    ScheduleStatus AS schedule_status,
+    nullIf(ResultStatus, 'NONE') AS result_status,
+    (HasLiveData != 'False') AS has_live_data,
+    (IsLive != 'False') AS is_live,
+    parseDateTime64BestEffort(StartTime, 3) AS start_time,
+    Description AS description,
+    ShortDescription AS short_description,
+    Location AS location,
+    nullIf(ResultsCredit, 'None') AS results_credit,
+    nullIf(TimingCredit, 'None') AS timing_credit,
+    (HasAnalysis != 'False') AS has_analysis,
+    StartMode AS start_mode,
+    CAST(NrShootings AS Int8) AS nr_shootings,
+    CAST(NrSpareRounds AS Int8) AS nr_spare_rounds,
+    (HasSpareRounds != 'False') AS has_spare_rounds,
+    CAST(PenaltySeconds AS Int32) AS penalty_seconds,
+    CAST(NrLegs AS Int8) AS nr_legs,
+    ShootingPositions AS shooting_positions,
+    CAST(LocalUTCOffset AS Int32) AS local_utc_offset,
+    RSC AS rsc,
+    nullIf(GenderOrder, 'None') AS gender_order,
+    CAST(season_id AS Int32) AS season_id,
+    stage,
+    CAST(rt AS Int32) AS rt
+FROM {{ source('biathlon_raw', 'competition') }}
+
