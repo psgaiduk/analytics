74dd07037742a27fdd9a944cdb31a828a7689cae
pavel <psgaiduk@gmail.com>
2026-01-12 13:38:56 +0300

    feat: change biathlon_results to biathlon_update_race_results

--- airflow/dags/biathlon_results.py
+++ airflow/dags/biathlon_results.py
@@ -3,15 +3,16 @@ from logging import getLogger
 
 from airflow.sdk import DAG, Param, task
 
-from constants import BIATHLON_RESULTS_URL
-from sdk.clickhouse_sdk import DeleteFromDatabase, GetDataByQuery, InsertDataFrame
+from functions.insert_values_to_database import load_to_database
+from sdk.biathlon.fetch_data import BiathlonResultsFetcher
+from sdk.clickhouse_sdk import DeleteFromDatabase
 
 
 log = getLogger(__name__)
 
 with DAG(
-    dag_id="biathlon_results",
-    schedule="0 */1 * * *",
+    dag_id="biathlon_update_race_results",
+    schedule=None,
     start_date=datetime(2024, 1, 1),
     catchup=False,
     tags=["biathlon"],
@@ -21,147 +22,27 @@ with DAG(
             type="integer",
             description="RT для biathlonresults.com",
         ),
-        "recreate": Param(
-            False,
-            type="boolean",
-            description="Recreate table in database or not",
+        "race_id": Param(
+            type="string",
+            description="Id гонки, которую нужно обновить.",
         ),
     },
 ) as dag:
 
     @task()
-    def get_race_id() -> int:
-        from clickhouse_connect.driver.exceptions import DatabaseError
-
-        query_for_get_race_id = """
-        SELECT
-            RaceId
-        FROM biathlon_raw.competition
-        WHERE RaceId NOT IN (SELECT DISTINCT race_id FROM biathlon_raw.result)
-        AND StatusId = '11'
-        ORDER BY StartTime DESC
-        LIMIT 1
-        """
-        try:
-            race_id_df = GetDataByQuery().get_data(query=query_for_get_race_id)
-        except DatabaseError as e:
-            log.error(f"Database error {e}")
-            query_for_get_race_id = """ 
-                SELECT
-                    RaceId
-                FROM biathlon_raw.competition
-                WHERE StatusId = '11'
-                ORDER BY StartTime DESC
-                LIMIT 1
-                """
-            race_id_df = GetDataByQuery().get_data(query=query_for_get_race_id)
-
-        log.info(f"Get race id from database race_id_df = {race_id_df}")
-        if race_id_df.empty:
-            log.warning("Race Id is empty")
-            return
-
-        race_id = race_id_df["RaceId"].iloc[0]
-        return race_id
-
-    @task()
-    def fetch_results(race_id: int, **kwargs) -> list:
+    def update_race_results(**kwargs) -> list:
         """Загружает данные соревнований с biathlonresults.com"""
-        from time import sleep
-
-        from pandas import DataFrame, concat
-        from requests import get
-
-        results = DataFrame()
-        analytic_results = DataFrame()
+        race_id = kwargs["params"]["race_id"]
         rt = kwargs["params"]["rt"]
+        recreate = False
 
-        url = f"{BIATHLON_RESULTS_URL}/Results?RT={rt}&RaceId={race_id}"
-        response = get(url, timeout=30)
-        log.info(f"Status code for race_id {race_id}: {response.status_code}")
-        if response.status_code != 200:
-            log.error(f"Get error response: {response.text}")
-            return
+        table_results = "biathlon_raw.result"
+        table_analytics_results = "biathlon_raw.analytics_result"
+        DeleteFromDatabase(table_name=table_results).delete_where(condition=f"race_id = '{race_id}'")
+        DeleteFromDatabase(table_name=table_analytics_results).delete_where(condition=f"race_id = '{race_id}'")
 
-        data = response.json()
-        if not data:
-            return
+        results, analytics_results = BiathlonResultsFetcher().fetch(race_id=race_id, rt=rt)
+        load_to_database(table_name=table_results, data=results, recreate=recreate)
+        load_to_database(table_name=table_analytics_results, data=analytics_results, recreate=recreate)
 
-        df = DataFrame(data["Results"])
-        df["race_id"] = race_id
-        df["rt"] = rt
-        results = concat([results, df], ignore_index=True)
-        sleep(1)
-
-        competition = data["Competition"]
-        legs = int(competition.get("NrLegs", 0))
-        shootings = int(competition.get("NrShootings", 0))
-        analytic_types = [
-            ["CRST", "Total Course Time"],
-            ["RNGT", "Total Range Time"],
-            ["STTM", "Total Shooting Time"],
-            ["SKIT", "Ski Time"],
-        ]
-
-        if legs:
-            analytic_types.extend([[f"FI{i + 1}L", f"Results Les {i + 1}"] for i in range(legs)])
-            analytic_types.extend([[f"CRST{i + 1}", f"Course Time Leg {i + 1}"] for i in range(legs)])
-            analytic_types.extend([[f"RNGT{i + 1}T", f"Range Time Leg {i + 1}"] for i in range(legs)])
-        else:
-            legs = 1
-
-        analytic_types.extend([[f"CRS{i + 1}", f"Course Time Lap {i + 1}"] for i in range((shootings + 1) * legs)])
-        analytic_types.extend([[f"RNG{i + 1}", f"Range Time {i + 1}"] for i in range(shootings * legs)])
-        analytic_types.extend([[f"S{i + 1}TM", f"Shooting Time {i + 1}"] for i in range(shootings * legs)])
-        log.info(f"analytic_types: {analytic_types}")
-
-        for type_id, type_name in analytic_types:
-            analytics_url = f"{BIATHLON_RESULTS_URL}/AnalyticResults?RaceId={race_id}&TypeId={type_id}"
-            response = get(analytics_url, timeout=30)
-            log.info(f"Status code for type_id {type_id}: {response.status_code}")
-            if response.status_code != 200:
-                log.error(f"Get error response: {response.text}")
-                continue
-            analytics_data = response.json()
-            if not analytics_data:
-                continue
-
-            df = DataFrame(analytics_data["Results"])
-            df["race_id"] = race_id
-            df["type_id"] = type_id
-            df["type_name"] = type_name
-            analytic_results = concat([analytic_results, df], ignore_index=True)
-
-            log.debug(f"data: {df.head(2)}")
-            sleep(1)
-
-        return [results, analytic_results]
-
-    @task
-    def load_to_clickhouse(race_id: int, all_results: list, **kwargs) -> None:
-        """
-        Сохраняет данные в ClickHouse
-        """
-
-        results, analytics_results = all_results
-        recreate = kwargs["params"]["recreate"]
-        log.info(f"Start load data to clickhouse, recreate={recreate}, {type(recreate)}")
-
-        if results.empty:
-            log.error("DataFrame пуст")
-        else:
-            table_name = "biathlon_raw.result"
-            DeleteFromDatabase(table_name=table_name).delete_where(condition=f"race_id = '{race_id}'")
-            InsertDataFrame(df=results, table_name=table_name).insert_data(recreate=recreate)
-
-        if analytics_results.empty:
-            log.error("Analytics DataFrame пуст")
-        else:
-            table_name = "biathlon_raw.analytics_result"
-            DeleteFromDatabase(table_name=table_name).delete_where(condition=f"race_id = '{race_id}'")
-            InsertDataFrame(df=analytics_results, table_name=table_name).insert_data(recreate=recreate)
-
-    race_id = get_race_id()
-    if race_id:
-        all_results = fetch_results(race_id=race_id)
-        load_to_clickhouse(race_id=race_id, all_results=all_results)
+    update_race_results()
