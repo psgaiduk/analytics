1643ed87112218b3409ab250bbad07e98c035f2a
pavel <psgaiduk@gmail.com>
2026-01-20 12:53:18 +0300

    feat: add biathlon_raw.events

--- airflow/dags/choices/name_tables.py
+++ airflow/dags/choices/name_tables.py
@@ -4,6 +4,7 @@ from enum import Enum
 class TableNames(str, Enum):
     """Name of tables in database."""
 
+    BIATHLON_EVENTS = "biathlon_raw.events"
     BIATHLON_COMPETITION = "biathlon_raw.competition"
     BIATHLON_RESULT = "biathlon_raw.result"
     BIATHLON_ANALYTICS_RESULT = "biathlon_raw.analytics_result"
