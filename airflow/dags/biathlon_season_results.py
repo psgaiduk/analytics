c47b3057d83eb9c787644b8f40e6e63170de0458
pavel <psgaiduk@gmail.com>
2026-01-21 14:25:59 +0300

    delete: delete not used code in biathlon season results

--- airflow/dags/biathlon_season_results.py
+++ airflow/dags/biathlon_season_results.py
@@ -1,17 +1,15 @@
 from datetime import date, datetime
 from logging import getLogger
-from time import sleep
 
 from airflow.exceptions import AirflowSkipException
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.sdk import DAG, Param, task
-from clickhouse_connect.driver.exceptions import DatabaseError
 from pandas import DataFrame, concat
 
 from choices.name_tables import TableNames
 from functions.insert_values_to_database import load_to_database
-from sdk.biathlon.fetch_data import BiathlonCompetitionsFetcher, BiathlonEventsFetcher, BiathlonResultsFetcher
-from sdk.clickhouse_sdk import GetDataByQuery, DeleteFromDatabase
+from sdk.biathlon.fetch_data import BiathlonCompetitionsFetcher, BiathlonEventsFetcher
+from sdk.clickhouse_sdk import DeleteFromDatabase
 
 
 log = getLogger(__name__)
@@ -89,65 +87,6 @@ with DAG(
             log.debug("Month after 6")
             season_id = f"{year}{year + 1}"
         return season_id
-
-    @task()
-    def get_results(**kwargs):
-        rt = kwargs["params"]["rt"]
-        season_id = kwargs["params"]["season_id"] or generate_season_id()
-
-        table_results = TableNames.BIATHLON_RESULT.value
-        table_analytics_results = TableNames.BIATHLON_ANALYTICS_RESULT.value
-        DeleteFromDatabase(table_name=table_results).delete_where(condition=f"season_id = '{season_id}'")
-        DeleteFromDatabase(table_name=table_analytics_results).delete_where(condition=f"season_id = '{season_id}'")
-
-        while True:
-            race_id = _get_race_id(season_id=season_id)
-            if not race_id:
-                log.info("Not races without results for season stop working.")
-                break
-            log.info(f"start update race id = {race_id}")
-            results, analytics_results = BiathlonResultsFetcher().fetch(season_id=season_id, race_id=race_id, rt=rt)
-            load_to_database(table_name=table_results, data=results)
-            load_to_database(table_name=table_analytics_results, data=analytics_results)
-            log.info(f"complete update race id = {race_id}")
-            sleep(5)
-
-    def _get_race_id(season_id: str) -> int:
-        log.info(f"Get season_id = {season_id}")
-        query_for_get_race_id = f"""
-        SELECT
-            RaceId
-        FROM {TableNames.BIATHLON_COMPETITION.value}
-        WHERE StatusId = '11' AND season_id = '{season_id}'
-        AND RaceId NOT IN (
-            SELECT
-                DISTINCT race_id
-            FROM {TableNames.BIATHLON_RESULT.value}
-            WHERE season_id = '{season_id}'
-        )
-        ORDER BY StartTime DESC
-        LIMIT 1
-        """
-        try:
-            race_id_df = GetDataByQuery().get_data(query=query_for_get_race_id)
-        except DatabaseError as e:
-            log.error(f"Database error {e}")
-            query_for_get_race_id = f"""
-                SELECT
-                    RaceId
-                FROM {TableNames.BIATHLON_COMPETITION.value}
-                WHERE StatusId = '11' AND season_id = '{season_id}'
-                ORDER BY StartTime DESC
-                LIMIT 1
-                """
-            race_id_df = GetDataByQuery().get_data(query=query_for_get_race_id)
-
-        log.info(f"Get race id from database race_id_df = {race_id_df}")
-        if race_id_df.empty:
-            log.warning("Race Id is empty")
-            return
-
-        return race_id_df["RaceId"].iloc[0]
 
     events_df = get_events()
     events_ids = load_events_to_clickhouse(events_df=events_df)
