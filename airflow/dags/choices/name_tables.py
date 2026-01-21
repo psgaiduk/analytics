78c36b987f705d6669324675f2aa3e856124850b
pavel <psgaiduk@gmail.com>
2026-01-21 13:44:25 +0300

    feat: rename tables for biathlon raw

--- airflow/dags/choices/name_tables.py
+++ airflow/dags/choices/name_tables.py
@@ -5,6 +5,6 @@ class TableNames(str, Enum):
     """Name of tables in database."""
 
     BIATHLON_EVENTS = "biathlon_raw.events"
-    BIATHLON_COMPETITION = "biathlon_raw.competition"
-    BIATHLON_RESULT = "biathlon_raw.result"
-    BIATHLON_ANALYTICS_RESULT = "biathlon_raw.analytics_result"
+    BIATHLON_COMPETITION = "biathlon_raw.competitions"
+    BIATHLON_RESULT = "biathlon_raw.results"
+    BIATHLON_ANALYTICS_RESULT = "biathlon_raw.analytics_results"
