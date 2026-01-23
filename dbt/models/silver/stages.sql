3ca8759e604e2376637c28b6e7f2ba0065fb05b7
pavel <psgaiduk@gmail.com>
2026-01-23 13:21:35 +0300

    fix: update stages by new data

--- dbt/models/silver/stages.sql
+++ dbt/models/silver/stages.sql
@@ -1,17 +1,21 @@
 SELECT
     season_id,
     ROW_NUMBER() OVER(PARTITION BY season_id ORDER BY MIN(start_time)) AS stage_num,
-    stage AS stage_name,
+    e.event_id AS event_id,
+    e.description AS event_name,
+    nat,
+    nat_long,
     location,
     MIN(start_time) AS start_stage,
     MAX(start_time) AS end_stage,
     COUNT(*) AS count_races,
-    (stage = 'OG__') AS is_olympic_games,
-    (stage = 'CH__') AS is_world_championship,
+    (lower(e.description) LIKE '%olympic%') AS is_olympic_games,
+    (lower(e.description) LIKE '%championships%') AS is_world_championship,
     CASE
         WHEN MIN(status_id) > 1 AND MAX(status_id) = 11 THEN 'finished'
         WHEN min(status_id) = 1 AND MAX(status_id) = 1 THEN 'planned'
         ELSE 'current'
     END AS stage_status
-FROM {{ ref('competitions') }}
-GROUP BY season_id, stage, location
+FROM {{ ref('competitions') }} AS c
+JOIN {{ ref('events') }} AS e USING(event_id)
+GROUP BY season_id, e.event_id, e.description, nat, nat_long, location
