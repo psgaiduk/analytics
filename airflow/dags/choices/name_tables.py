aab7e750c65ab191e640d4e7c7cad817e7e17a63
pavel <psgaiduk@gmail.com>
2026-01-26 13:35:38 +0300

    feat: add tables for old olympic games in TableNames

--- airflow/dags/choices/name_tables.py
+++ airflow/dags/choices/name_tables.py
@@ -8,3 +8,7 @@ class TableNames(str, Enum):
     BIATHLON_COMPETITION = "biathlon_raw.competitions"
     BIATHLON_RESULT = "biathlon_raw.results"
     BIATHLON_ANALYTICS_RESULT = "biathlon_raw.analytics_results"
+
+    BIATHLON_OG_EVENTS = "biathlon_raw.events_og"
+    BIATHLON_OG_COMPETITION = "biathlon_raw.competitions_og"
+    BIATHLON_OG_RESULT = "biathlon_raw.results_og"
