e888d769ab669b63fa96171f761f6da82ccf4a99
pavel <psgaiduk@gmail.com>
2026-01-28 12:04:33 +0300

    feat: add old olympic competitions for bronze competitions

--- dbt/models/bronze/competitions.sql
+++ dbt/models/bronze/competitions.sql
@@ -35,3 +35,37 @@ SELECT
     parseDateTime64BestEffort(updated_at, 3) AS updated_at
 FROM {{ source('biathlon_raw', 'competitions') }}
 
+UNION ALL
+
+SELECT
+    RaceId AS race_id,
+    nullIf(km, '') AS km,
+    catId As cat_id,
+    DisciplineId AS discipline_id,
+    CAST(StatusId AS Int64) AS status_id,
+    StatusText AS status_text,
+    ScheduleStatus AS schedule_status,
+    nullIf(ResultStatus, 'nan') AS result_status,
+    (HasLiveData != 'False') AS has_live_data,
+    (IsLive != 'False') AS is_live,
+    parseDateTime64BestEffort(StartTime, 3) AS start_time,
+    Description AS description,
+    ShortDescription AS short_description,
+    Location AS location,
+    nullIf(ResultsCredit, 'nan') AS results_credit,
+    nullIf(TimingCredit, 'nan') AS timing_credit,
+    (HasAnalysis != 'False') AS has_analysis,
+    StartMode AS start_mode,
+    CAST(NrShootings AS Int8) AS nr_shootings,
+    CAST(NrSpareRounds AS Int8) AS nr_spare_rounds,
+    (HasSpareRounds != 'False') AS has_spare_rounds,
+    CAST(PenaltySeconds AS Int32) AS penalty_seconds,
+    CAST(NrLegs AS Int8) AS nr_legs,
+    ShootingPositions AS shooting_positions,
+    CAST(LocalUTCOffset AS Int32) AS local_utc_offset,
+    RSC AS rsc,
+    nullIf(GenderOrder, 'nan') AS gender_order,
+    season_id,
+    event_id,
+    parseDateTime64BestEffort(updated_at, 3) AS updated_at
+FROM {{ source('biathlon_raw', 'competitions_og') }}
